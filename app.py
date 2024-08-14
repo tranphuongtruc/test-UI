89ab556dd94138e7e9739ed4058f70871da88bba
tranphuongtruc <tranphuongtruc08@gmail.com>
2024-08-14 15:09:43 +0700

    Activate .html files using Flask

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+
+app = Flask(__name__, static_folder='static', template_folder='templates')
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/image-to-image')
+def image_to_image():
+    return render_template('image-to-image.html')
+
+
+@app.route('/text-to-image')
+def text_to_image():
+    return render_template('text-to-image.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'file' not in request.files:
+        return redirect(request.url)
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return redirect(request.url)
+
+    if file:
+        # Save the file to the 'uploads' folder
+        upload_folder = 'uploads'
+        if not os.path.exists(upload_folder):
+            os.makedirs(upload_folder)
+
+        file_path = os.path.join(upload_folder, file.filename)
+        file.save(file_path)
+        return 'Successfully uploaded'
+
+    return 'Failed to upload image.'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
